7b093fee4c4145b94e368fa34c8c946b6a0dcf07
RodsyTahmid <ms.rodsy.tahmid@g.bracu.ac.bd>
2024-02-20 06:40:45 +0600

    frontend

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AdminController; 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\VendorController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -11,18 +11,30 @@ use App\Http\Controllers\VendorController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
-*/
-
+*/ 
+ 
 Route::get('/', function () {
     return view('frontend.index');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth'])->group(function() {
+    
+Route::get('/dashboard', [UserController::class, 'UserDashboard'])->name('dashboard');
+
+
+}); // Gorup Milldeware End
+
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
+/// Admin Dashboard
 
 Route::middleware(['auth','role:admin'])->group(function() {
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashobard');
@@ -39,29 +51,25 @@ Route::middleware(['auth','role:admin'])->group(function() {
 
 });
 
+
 /// Vendor Dashboard
 Route::middleware(['auth','role:vendor'])->group(function() {
+   Route::get('/vendor/dashboard', [VendorController::class, 'VendorDashboard'])->name('vendor.dashobard');
 
-    Route::get('/vendor/dashboard', [VendorController::class, 'VendorDashboard'])->name('vendor.dashobard');
- 
-    Route::get('/vendor/logout', [VendorController::class, 'VendorDestroy'])->name('vendor.logout');
- 
-    Route::get('/vendor/profile', [VendorController::class, 'VendorProfile'])->name('vendor.profile');
- 
-     Route::post('/vendor/profile/store', [VendorController::class, 'VendorProfileStore'])->name('vendor.profile.store');
- 
-     Route::get('/vendor/change/password', [VendorController::class, 'VendorChangePassword'])->name('vendor.change.password');
- 
-     Route::post('/vendor/update/password', [VendorController::class, 'VendorUpdatePassword'])->name('vendor.update.password');
-    });
+   Route::get('/vendor/logout', [VendorController::class, 'VendorDestroy'])->name('vendor.logout');
+
+   Route::get('/vendor/profile', [VendorController::class, 'VendorProfile'])->name('vendor.profile');
+
+    Route::post('/vendor/profile/store', [VendorController::class, 'VendorProfileStore'])->name('vendor.profile.store');
+
+    Route::get('/vendor/change/password', [VendorController::class, 'VendorChangePassword'])->name('vendor.change.password');
+
+    Route::post('/vendor/update/password', [VendorController::class, 'VendorUpdatePassword'])->name('vendor.update.password');
+
+
+
+});
+
 
 Route::get('/admin/login', [AdminController::class, 'AdminLogin']);
 Route::get('/vendor/login', [VendorController::class, 'VendorLogin']);
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
