0a03f84440d0826c080749f08c2140c258263f16
RodsyTahmid <ms.rodsy.tahmid@g.bracu.ac.bd>
2024-03-26 00:25:14 +0600

    Category Backend

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\VendorController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\Backend\BrandController;
+use App\Http\Controllers\Backend\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -91,6 +92,18 @@ Route::controller(BrandController::class)->group(function(){
     Route::get('/all/brand' , 'AllBrand')->name('all.brand');
     Route::get('/add/brand' , 'AddBrand')->name('add.brand');
     Route::post('/store/brand' , 'StoreBrand')->name('store.brand');
+    Route::get('/edit/brand/{id}' , 'EditBrand')->name('edit.brand');
+    Route::post('/update/brand' , 'UpdateBrand')->name('update.brand');
+    Route::get('/delete/brand/{id}' , 'DeleteBrand')->name('delete.brand');
+
+});
+
+
+ // Category All Route 
+Route::controller(CategoryController::class)->group(function(){
+    Route::get('/all/category' , 'AllCategory')->name('all.category');
+    Route::get('/add/category' , 'AddCategory')->name('add.category');
+    Route::post('/store/category' , 'StoreCategory')->name('store.category');
     Route::get('/edit/brand/{id}' , 'EditBrand')->name('edit.brand');
     Route::post('/update/brand' , 'UpdateBrand')->name('update.brand');
     Route::get('/delete/brand/{id}' , 'DeleteBrand')->name('delete.brand');
