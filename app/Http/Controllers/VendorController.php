057ce7fb5501d58e0514fc95d6a438b920822ab8
RodsyTahmid <ms.rodsy.tahmid@g.bracu.ac.bd>
2024-03-26 15:26:44 +0600

    Vendor Register Page

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -6,9 +6,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
- 
-use App\Notifications\VendorRegNotification;
-use Illuminate\Support\Facades\Notification;
 
  
 class VendorController extends Controller
@@ -113,39 +110,6 @@ public function VendorUpdatePassword(Request $request){
     } // End Mehtod 
 
 
-      public function VendorRegister(Request $request) {
-       
-        $vuser = User::where('role','admin')->get();
-
-
-        $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'confirmed'],
-        ]);
-
-        $user = User::insert([ 
-            'name' => $request->name,
-            'username' => $request->username,
-            'email' => $request->email,
-            'phone' => $request->phone,
-            'vendor_join' => $request->vendor_join,
-            'password' => Hash::make($request->password),
-            'role' => 'vendor',
-            'status' => 'inactive',
-        ]);
-
-          $notification = array(
-            'message' => 'Vendor Registered Successfully',
-            'alert-type' => 'success'
-        );
-
-       Notification::send($vuser, new VendorRegNotification($request));
-        return redirect()->route('vendor.login')->with($notification);
-       
-    }// End Mehtod 
-
-
-
 
 }
+ 
