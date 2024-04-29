1be18e42489cf2d37662e4f65d3eaf623ca2e5eb
RodsyTahmid <ms.rodsy.tahmid@g.bracu.ac.bd>
2024-04-29 12:35:08 +0600

    cart

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ use App\Http\Controllers\Backend\BannerController;
 
 use App\Http\Controllers\Frontend\IndexController;
 use App\Http\Controllers\Frontend\CartController;
+
+use App\Http\Controllers\User\WishlistController; 
+use App\Http\Controllers\User\CompareController;
  
 /*
 |--------------------------------------------------------------------------
@@ -253,3 +256,49 @@ Route::get('/minicart/product/remove/{rowId}', [CartController::class, 'RemoveMi
 
 /// Add to cart store data For Product Details Page 
 Route::post('/dcart/data/store/{id}', [CartController::class, 'AddToCartDetails']);
+
+/// Add to Wishlist 
+Route::post('/add-to-wishlist/{product_id}', [WishlistController::class, 'AddToWishList']);
+
+/// Add to Compare 
+Route::post('/add-to-compare/{product_id}', [CompareController::class, 'AddToCompare']);
+
+
+
+/// User All Route
+Route::middleware(['auth','role:user'])->group(function() {
+
+ // Wishlist All Route 
+Route::controller(WishlistController::class)->group(function(){
+    Route::get('/wishlist' , 'AllWishlist')->name('wishlist');
+    Route::get('/get-wishlist-product' , 'GetWishlistProduct');
+    Route::get('/wishlist-remove/{id}' , 'WishlistRemove'); 
+
+}); 
+
+
+ // Compare All Route 
+Route::controller(CompareController::class)->group(function(){
+    Route::get('/compare' , 'AllCompare')->name('compare');
+    Route::get('/get-compare-product' , 'GetCompareProduct');
+   Route::get('/compare-remove/{id}' , 'CompareRemove'); 
+
+}); 
+
+
+ // Cart All Route 
+Route::controller(CartController::class)->group(function(){
+    Route::get('/mycart' , 'MyCart')->name('mycart');
+    Route::get('/get-cart-product' , 'GetCartProduct');
+    Route::get('/cart-remove/{rowId}' , 'CartRemove');
+
+    Route::get('/cart-decrement/{rowId}' , 'CartDecrement');
+    Route::get('/cart-increment/{rowId}' , 'CartIncrement');
+    
+
+}); 
+
+
+
+
+}); // end group User middleware
